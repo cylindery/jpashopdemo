d4ec75ef4bbf2d13c7311abd19d05d66c7f3bf85
juwonju <jxwxnjx@gmail.com>
2023-04-04 17:58:32 +0900

    실전 예제 2 - 연관관계 매핑 시작

--- src/main/java/jpabook/jpashopdemo/domain/Order.java
+++ src/main/java/jpabook/jpashopdemo/domain/Order.java
@@ -2,22 +2,34 @@ package jpabook.jpashopdemo.domain;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "ORDERS")
 public class Order {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime orderDate;
 
     @Enumerated(value = EnumType.STRING)
     private OrderStatus status;
+
+    public void addOrderItem(OrderItem orderItem) { // 연관관계 편의 메서드
+        orderItems.add(orderItem);
+        orderItem.setOrder(this); // 양방향 연관관계
+    }
 
     public Long getId() {
         return id;
@@ -27,12 +39,12 @@ public class Order {
         this.id = id;
     }
 
-    public Long getMemberId() {
-        return memberId;
+    public Member getMember() {
+        return member;
     }
 
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
+    public void setMember(Member member) {
+        this.member = member;
     }
 
     public LocalDateTime getOrderDate() {
